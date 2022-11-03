69845d6f90eb4148a8522fcb158a859f5938d557
Federico <fedeaacevedo@gmail.com>
2022-11-03 09:42:27 -0300

    Edit

--- Turnos/Controllers/EspecialidadController.cs
+++ Turnos/Controllers/EspecialidadController.cs
@@ -19,12 +19,38 @@ namespace Turnos.Controllers
 
         public IActionResult Edit(int? id)
         {
-            if(id == null)
+            //Si el id no existe generamos un error generico
+            if (id == null)
             {
                 return NotFound();
             }
             var especialidad = _context.Especialidad.Find(id);
+
+            //Si la especialidad no existe o no es indicada por el usuario generamos un error generico
+            if (especialidad == null)
+            {
+                return NotFound();
+            }
+
             return View(especialidad);
         }
+
+        [HttpPost] // Esto diferencia el metodo Edit que graba, del edit de vista.
+        public IActionResult Edit(int id, [Bind("IdEspecialidad, Descripcion")] Especialidad especialidad)
+        {
+            if(id != especialidad.IdEspecialidad)
+            {
+                return NotFound();
+            }
+            //Si la informacion que cargamos es correcta, lo actualizamos o lo guardamos
+            if(ModelState.IsValid)
+            {
+                _context.Update(especialidad);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(especialidad);
+        }
+
     }
 }
