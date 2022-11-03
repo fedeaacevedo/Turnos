8a3243595545cec80f127ef899c139bbbff8cc0b
Federico <fedeaacevedo@gmail.com>
2022-11-02 23:00:55 -0300

    Agregar archivos de proyecto.

---
+++ Turnos/Controllers/EspecialidadController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Turnos.Models;
+
+namespace Turnos.Controllers
+{
+    public class EspecialidadController : Controller
+    {
+
+        public EspecialidadController(TurnosContext context)
+        {
+
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
