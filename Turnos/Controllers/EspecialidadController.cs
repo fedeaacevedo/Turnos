76818c4dc4cc357ab03e8271bd0ab1c1a4ed5ba7
Federico <fedeaacevedo@gmail.com>
2022-11-02 23:34:47 -0300

    Edit

--- Turnos/Controllers/EspecialidadController.cs
+++ Turnos/Controllers/EspecialidadController.cs
@@ -1,19 +1,30 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 using Turnos.Models;
 
 namespace Turnos.Controllers
 {
     public class EspecialidadController : Controller
     {
-
+        private readonly TurnosContext _context;
         public EspecialidadController(TurnosContext context)
         {
-
+            _context = context;
         }
 
         public IActionResult Index()
         {
-            return View();
+            return View(_context.Especialidad.ToList());
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if(id == null)
+            {
+                return NotFound();
+            }
+            var especialidad = _context.Especialidad.Find(id);
+            return View(especialidad);
         }
     }
 }
